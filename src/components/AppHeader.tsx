23c2617cc0a32aaf0b451ba8e34eaab44e7e14f7
ggm77 <shmshm86@naver.com>
2026-07-15 22:48:04 +0900

    Right-align header nav on desktop
    Split the nav out of the logo wrapper so the flex space-between
    pushes the menu to the far right instead of hugging the logo.

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -15,26 +15,22 @@ export default function AppHeader() {
 
   return (
     <header className="app-header">
-      <div className="app-header__left">
-        <div className="app-header__logo">
-          <img className="app-header__logo-icon" src="/logo2.svg" alt="" />
-          <span>뽀모도로 땅따먹기</span>
-        </div>
-        <nav className={'app-header__nav' + (menuOpen ? ' app-header__nav--open' : '')}>
-          {NAV_ITEMS.map((item) => (
-            <NavLink
-              key={item.to}
-              to={item.to}
-              onClick={() => setMenuOpen(false)}
-              className={({ isActive }) =>
-                'app-header__nav-item' + (isActive ? ' app-header__nav-item--active' : '')
-              }
-            >
-              {item.label}
-            </NavLink>
-          ))}
-        </nav>
+      <div className="app-header__logo">
+        <img className="app-header__logo-icon" src="/logo2.svg" alt="" />
+        <span>뽀모도로 땅따먹기</span>
       </div>
+      <nav className={'app-header__nav' + (menuOpen ? ' app-header__nav--open' : '')}>
+        {NAV_ITEMS.map((item) => (
+          <NavLink
+            key={item.to}
+            to={item.to}
+            onClick={() => setMenuOpen(false)}
+            className={({ isActive }) => 'app-header__nav-item' + (isActive ? ' app-header__nav-item--active' : '')}
+          >
+            {item.label}
+          </NavLink>
+        ))}
+      </nav>
       <button
         type="button"
         className="app-header__menu-btn"
